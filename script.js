aa00540ac7e684f828bc0f530e6d546b033e5e08
Gustavo <gustavossw@hotmail.com>
2022-03-23 11:50:26 -0300

    nav-bar added!!!!!

---
+++ script.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', function() {
+    navBar.classList.toggle('active');
+    console.log(navBar)
+})
