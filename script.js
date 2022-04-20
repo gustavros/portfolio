a516a144db6bf2429a6276add3f6f5512dc06eb2
Gustavo <gustavossw@hotmail.com>
2022-04-20 07:44:49 -0300

    update indent

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navBar = document.querySelector('.navbar');
 
-navToggle.addEventListener('click', function() {
-    navBar.classList.toggle('active');
+navToggle.addEventListener('click', function () {
+  navBar.classList.toggle('active');
+});
 
-})
-
-navBar.addEventListener('click', function() {
-    navBar.classList.remove('active');
-})
+navBar.addEventListener('click', function () {
+  navBar.classList.remove('active');
+});
